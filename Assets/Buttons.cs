57e49d62c8916fa5030ebcaf8729f057640dfdaa
Lucifer54 <nandamsreenivasulu2017@gmail.com>
2021-01-05 15:06:01 +0530

    Master and corresponding scenes on clicking buttons

---
+++ Assets/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public Button button;
+    public string newscene;
+    public void Start()
+    {  // writing this statement in the start rather than in the update because unity has the standard approaches to take input from the keys
+        button.onClick.AddListener(OnButtonClick);
+        
+    }
+    public void OnButtonClick() {
+        SceneManager.LoadScene(newscene);
+    }
+}
